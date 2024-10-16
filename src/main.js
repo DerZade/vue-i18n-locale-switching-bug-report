30799732a7b59fcd3684e0383aa527703a2b6ac2
DerZade <DerZade@gmail.com>
2024-10-16 17:37:43 +0200

    install i18n

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const i18n = createI18n({
+    locale: 'ja',
+    messages: {
+        en: {
+            hello: 'hello world'
+        },
+        ja: {
+            hello: 'こんにちは、世界'
+        }
+    }
+})
+
+createApp(App).use(i18n).mount('#app')
